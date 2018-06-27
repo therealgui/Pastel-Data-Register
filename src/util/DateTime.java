240604ce62ed93b7eda92d5e62a8d62d2b929c5a
therealgui <therealgui94@gmail.com>
2018-06-27 17:45:34 +0100

    Adds RecordTest test class Changes propertie date of class Record from Calendar to LocalDate, using new java API java.time Comments all content of DateTime java class

--- src/util/DateTime.java
+++ src/util/DateTime.java
@@ -1,3 +1,4 @@
+/*
 package util;
 
 import java.text.ParseException;
@@ -14,83 +15,99 @@ public final class DateTime {
 		//set it to private to make this an utility class
 	}
 	
-	/**
+	*/
+/**
 	 * returns the current date of the system
 	 * 
 	 * @return GregorianCalendar object
-	 */
+	 *//*
+
 	public static Calendar now() {
 		return new GregorianCalendar();
 	}
 	
-	/**
+	*/
+/**
 	 * returns the number of the week in the year given a certain date
 	 * 
 	 * @param date Calendar instance
 	 * @return week number
-	 */
+	 *//*
+
 	public static int weekNumber(final Calendar date) {
 		return date.get(Calendar.WEEK_OF_YEAR);
 	}
 	
-	/**
+	*/
+/**
 	 * returns the current month of the current date of the system
 	 * 
 	 * @return current month
-	 */
+	 *//*
+
 	public static int currentMonth() {
 		return now().get(Calendar.MONTH);
 	}
 	
-	/**
+	*/
+/**
 	 * returns the current year of the current date of the system
 	 * 
 	 * @return current year
-	 */
+	 *//*
+
 	public static int currentYear() {
 		return now().get(Calendar.YEAR);
 	}
 	
-	/**
+	*/
+/**
 	 * checks if the two Calendar instances represent dates of the same month
 	 * 
 	 * @param a
 	 * @param b
 	 * @return boolean
-	 */
+	 *//*
+
 	public static boolean isSameMonth(final Calendar a, final Calendar b) {
 		return a.get(Calendar.MONTH) == b.get(Calendar.MONTH);
 	}
 	
-	/**
+	*/
+/**
 	 * checks if the two Calendar instances represent dates of the same year
 	 * @param a
 	 * @param b
 	 * @return boolean
-	 */
+	 *//*
+
 	public static boolean isSameYear(final Calendar a, final Calendar b) {
 		return a.get(Calendar.YEAR) == b.get(Calendar.YEAR);
 	}
 	
-	/**
+	*/
+/**
 	 * converts a Date instance to a Calendar instance
 	 * 
 	 * @param date
 	 * @return Calendar instance
-	 */
+	 *//*
+
 	public static Calendar dateToCalendar(final Date date) {
 		final Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return cal;
 	}
 	
-	/**
+	*/
+/**
 	 * parses a string that contains a date in a certain format
 	 * 
 	 * @param aDateString
 	 * @param format
 	 * @return a Date object or null if there was an error parsing the string
-	 */
+	 *//*
+
 	public static Calendar parseDate(final String aDateString, final String format) {
 		try {
 			final SimpleDateFormat df = new SimpleDateFormat(format);
@@ -103,38 +120,45 @@ public final class DateTime {
 		return null;
 	}
 	
-	/**
+	*/
+/**
 	 * format a given date to the specific date format YYYY/MM/dd
 	 * 
 	 * @param aDate Calendar instance
 	 * @return String containing the date formatted
-	 */
+	 *//*
+
 	public static String format(final Calendar aDate) {
 		return format(aDate, "YYYY/MM/dd");
 	}
 	
-	/**
+	*/
+/**
 	 * format a given date to a specific date format
 	 * 
 	 * @param aDate Calendar instance
 	 * @param dateFormat
 	 * @return String containing the date formatted
-	 */
+	 *//*
+
 	public static String format(final Calendar aDate, String dateFormat) {
 		final SimpleDateFormat formater = new SimpleDateFormat(dateFormat);
 		return formater.format(aDate.getTime());
 	}
 	
-	/**
+	*/
+/**
 	 * compare if two dates are the same
 	 * 
 	 * @param dateA
 	 * @param dateB
 	 * @return boolean
-	 */
+	 *//*
+
 	public static boolean compareDates(Calendar dateA, Calendar dateB) {
 		return (dateA.get(Calendar.DAY_OF_MONTH) == dateB.get(Calendar.DAY_OF_MONTH)
 				&& dateA.get(Calendar.MONTH) == dateB.get(Calendar.MONTH)
 				&& dateA.get(Calendar.YEAR) == dateB.get(Calendar.YEAR));
 	}
 }
+*/
--- test/model/RecordTest.java
+++ test/model/RecordTest.java
@@ -3,7 +3,10 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,8 +24,7 @@ class RecordTest {
 		double despesaFatura = 10;
 		double despesa = 10;
 		double iva = 23;
-		Calendar date = DateTime.now();
-		
+		LocalDate date = LocalDate.of(2018, Month.MARCH, 1);
 		record1 = new Record(receitaDiaria, despesaFatura, despesa, iva, date);
 		record2 = new Record(receitaDiaria, despesaFatura, despesa, iva, date);
 	}
@@ -34,16 +36,36 @@ class RecordTest {
 
 	@Test
 	void equals_ObjectsAreNotTheSameDiffDates_False() {
-		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.WEEK_OF_YEAR,3);
-		cal.set(Calendar.DAY_OF_WEEK,4);
-		record2.setDate(cal);
+		LocalDate date = LocalDate.of(2018, Month.FEBRUARY, 1);
+		record2.setDate(date);
 		assertFalse(record1.equals(record2));
 	}
 
 	@Test
-	void testCompareTo() {
-		fail("Not yet implemented");
+	void equals_ObjectsEqualSameReference_True(){
+		Record record3 = record1;
+		assertTrue(record1.equals(record3));
+	}
+
+	@Test
+	void compareTo_SameDate_True() {
+		assertTrue(record1.compareTo(record2) == 0);
+	}
+
+	@Test
+	void compareTo_GreaterDate_True(){
+		LocalDate date = record2.getDate();
+		date = date.minusMonths(1);
+		record2.setDate(date);
+		assertTrue(record1.compareTo(record2) == 1);
+	}
+
+	@Test
+	void compareTo_LesserDate_True(){
+		LocalDate date = record2.getDate();
+		date = date.plusMonths(1);
+		record2.setDate(date);
+		assertTrue(record1.compareTo(record2) == -1);
 	}
 
 }
