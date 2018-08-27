87000f554ba5b84982614915f1612075bef932d3
therealgui <gui.carvalho94@gmail.com>
2018-08-27 14:07:48 +0100

    Updates relational-schema image Adds new fields to monthlyRecords table

--- db/db.sql
+++ db/db.sql
@@ -2,14 +2,22 @@ drop table if exists monthlyRecord;
 drop table if exists record;
 
 create table record(
-    id int primary key,
+    id int primary key auto_increment,
     daily_revenue float,
     bill_expense float,
     expense float,
     tax float,
-    entry_date date
+    entry_date date unique
 );
 
-create table montlyRecord(
-    id_record int foreign key
+create table monthlyRecord(
+    id_record int,
+    month_number int,
+    year int
 );
+
+alter table monthlyRecord add foreign key (id_record) references record(id);
+
+insert into record(daily_revenue,bill_expense,expense,tax,entry_date) values(5.5,5.6,5.7,5.8,to_date('2018-08-18','yyyy-mm-dd'));
+
+select * from record;
